f0bc31c8429ea4ffcd782219437bb01f5f76797f
Eugene Savitsky <eugenesavitsky89@gmail.com>
2022-05-03 01:08:37 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  constructor (type) {
+    this.reverseType = type === false;
+}
+encrypt (message, key) {
+    if (message === undefined || key === undefined) throw new Error('Incorrect arguments!')
+    let shift = 0;
+    key = key.toUpperCase().split("").map(c => c.charCodeAt(0) - 65);
+    message = message.toUpperCase().split("").map((item, i) => (/[A-Z]/.test(item))
+            ? String.fromCharCode((item.charCodeAt(0) - 65 + key[(i - shift) % key.length]) % 26 + 65)
+            : (shift++, item));
+
+    return this.reverseType ? message.reverse().join("") : message.join("")
+}
+
+decrypt (message, key) {
+    if (message === undefined || key === undefined) throw new Error('Incorrect arguments!')
+    let shift = 0;
+    key = key.toUpperCase().split("").map(c => c.charCodeAt(0) - 65);
+    message = message.toUpperCase().split("").map((item, i) => (/[A-Z]/.test(item))
+            ? String.fromCharCode((item.charCodeAt(0) - 65 + 26 - key[(i - shift) % key.length])
+                    % 26 + 65)
+            : (shift++, item));
+
+    return this.reverseType ? message.reverse().join("") : message.join("");
+}
 }
 
 module.exports = {
